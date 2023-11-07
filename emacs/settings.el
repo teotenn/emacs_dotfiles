eff7db8070f27aa1ee1f27f5e755472240791cb6
teodorm3 <manuel.teodoro_tenango@contractors.roche.com>
2023-11-07 10:43:01 +0100

    Closing testing except for consult

--- emacs/settings.el
+++ emacs/settings.el
@@ -86,6 +86,9 @@
 ;; From emacs 29.1
 (setq show-paren-context-when-offscreen 'overlay)
 
+;; fill columns to 80
+(setq fill-column 80)
+
 ;; personal function for windows
 (defun tt/wrap ()
   "Shortcut to open neotree directly on wrapper"
@@ -681,6 +684,36 @@ utils::assignInNamespace(\"q\",
 (setq tab-bar-close-button-show nil)
 (setq tab-bar-new-button-show nil)
 
+;;; Vertico
+(when (require 'vertico nil :noerror)
+  (require 'vertico-directory)
+  ;; Cycle back to top/bottom result when the edge is reached
+  (customize-set-variable 'vertico-cycle t)
+
+  ;; Start Vertico
+  (vertico-mode 1))
+
+;; To allow vertico keep history
+(use-package savehist
+:init
+(savehist-mode))
+
+;;; Marginalia
+(when (require 'marginalia nil :noerror)
+  ;; Configure Marginalia
+  (customize-set-variable 'marginalia-annotators
+			  '(marginalia-annotators-heavy
+			    marginalia-annotators-light
+			    nil))
+  (marginalia-mode 1))
+
+;;; Orderless
+(when (require 'orderless nil :noerror)
+  ;; Set up Orderless for better fuzzy matching
+  (customize-set-variable 'completion-styles '(orderless basic))
+  (customize-set-variable 'completion-category-overrides
+			  '((file (styles . (partial-completion))))))
+
 (use-package which-key
   :config
   (which-key-mode))
@@ -769,45 +802,6 @@ utils::assignInNamespace(\"q\",
 
 ;; Silence compiler warnings as they can be pretty disruptive (setq comp-async-report-warnings-errors nil)
 
-;;; Vertico
-(when (require 'vertico nil :noerror)
-  (require 'vertico-directory)
-  ;; Cycle back to top/bottom result when the edge is reached
-  (customize-set-variable 'vertico-cycle t)
-
-  ;; Start Vertico
-  (vertico-mode 1))
-
-;; The code below is not necessary because those are turn off by default
-;; on emacs, however they conflict with vertico and orderless so
-;; Is good to remember to turn them off.
-  ;; (with-eval-after-load 'crafted-defaults-config
-  ;;   (fido-mode -1)
-  ;;   (fido-vertical-mode -1)
-  ;;   (icomplete-mode -1)
-  ;;   (icomplete-vertical-mode -1)))
-
-;; To allow vertico keep history
-(use-package savehist
-:init
-(savehist-mode))
-
-;;; Marginalia
-(when (require 'marginalia nil :noerror)
-  ;; Configure Marginalia
-  (customize-set-variable 'marginalia-annotators
-			  '(marginalia-annotators-heavy
-			    marginalia-annotators-light
-			    nil))
-  (marginalia-mode 1))
-
-;;; Orderless
-(when (require 'orderless nil :noerror)
-  ;; Set up Orderless for better fuzzy matching
-  (customize-set-variable 'completion-styles '(orderless basic))
-  (customize-set-variable 'completion-category-overrides
-			  '((file (styles . (partial-completion))))))
-
 ;; Since Consult doesn't need to be required, we assume the user wants these
 ;; setting if it is installed (regardless of the installation method).
 (when (locate-library "consult")
@@ -816,64 +810,3 @@ utils::assignInNamespace(\"q\",
   (keymap-set minibuffer-local-map "C-r" 'consult-history)
 
   (setq completion-in-region-function #'consult-completion-in-region))
-
-;; Embark
-(when (require 'embark nil :noerror)
-
-  (keymap-global-set "<remap> <describe-bindings>" #'embark-bindings)
-  (keymap-global-set "C-." 'embark-act)
-
-  ;; Use Embark to show bindings in a key prefix with `C-h`
-  (setq prefix-help-command #'embark-prefix-help-command)
-
-  (when (require 'embark-consult nil :noerror)
-    (with-eval-after-load 'embark-consult
-      (add-hook 'embark-collect-mode-hook #'consult-preview-at-point-mode))))
-
-;;; Corfu
-;; (when (require 'corfu nil :noerror)
-
-;;   (unless (display-graphic-p)
-;;     (when (require 'corfu-terminal nil :noerror)
-;; 	(corfu-terminal-mode +1)))
-
-;;   ;; Setup corfu for popup like completion
-;;   (customize-set-variable 'corfu-cycle t)        ; Allows cycling through candidates
-;;   (customize-set-variable 'corfu-auto t)         ; Enable auto completion
-;;   (customize-set-variable 'corfu-auto-prefix 2)  ; Complete with less prefix keys
-
-;;   (global-corfu-mode 1)
-;;   (when (require 'corfu-popupinfo nil :noerror)
-
-;;     (corfu-popupinfo-mode 1)
-;;     (eldoc-add-command #'corfu-insert)
-;;     (keymap-set corfu-map "M-p" #'corfu-popupinfo-scroll-down)
-;;     (keymap-set corfu-map "M-n" #'corfu-popupinfo-scroll-up)
-;;     (keymap-set corfu-map "M-d" #'corfu-popupinfo-toggle)))
-
-(when (require 'cape nil :noerror)
-;; Setup Cape for better completion-at-point support and more
-
-;; Add useful defaults completion sources from cape
-(add-to-list 'completion-at-point-functions #'cape-file)
-(add-to-list 'completion-at-point-functions #'cape-dabbrev)
-
-;; Silence the pcomplete capf, no errors or messages!
-;; Important for corfu
-(advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
-
-;; Ensure that pcomplete does not write to the buffer
-;; and behaves as a pure `completion-at-point-function'.
-(advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify))
-
-;; To explore the section below we need to understand corfu well
-;; For now it is conflicting with my previous eshell autocomplete
-;;
-;; No auto-completion or completion-on-quit in eshell
-;; (defun crafted-completion-corfu-eshell ()
-;;   "Special settings for when using corfu with eshell."
-;;   (setq-local corfu-quit-at-boundary t
-;;               corfu-quit-no-match t
-;;               corfu-auto nil)
-;;   (corfu-mode))
-;; (add-hook 'eshell-mode-hook #'crafted-completion-corfu-eshell))
